157f7511b3aae9a536dc52e277e0fb2ad5886f00
SwathiKrishna93 <swathikrishna.deepu@gmail.com>
2016-11-09 15:10:26 +0530

    Rooms Added, Usernames reflect changes
    Private messaging is still possible

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var server = require('http').createServer(app);
 var io= require('socket.io').listen(server);
 //var mongoose = require('mongoose');
 var users={};
-
+//var fs = require('fs');
 server.listen(3000);
 
+var rooms=['Room 1','Room 2','Room 3','Room 4', 'Room 5'];
 // mongoose.connect('mongodb://localhost/chat', function(err){
 // 	if(err){
 // 		console.log(err);
@@ -23,6 +24,8 @@ server.listen(3000);
 // });
 
 // var Chat = mongoose.model('Message',chatSchema);
+var roomusers;
+// = [[],[],[],[],[]];
 
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
@@ -36,19 +39,36 @@ io.sockets.on('connection',function(socket){
 	//	socket.emit('load old messages',docs);
 	//});
 	//socket.join('some room');
+	
 	socket.on('new user', function(data,callback){
-		if (data in users) {
+		if (data in users || data=='SERVER') {
 			callback(false);
 			}
 		else{
 			callback(true);
 			socket.nickname = data;
+			socket.room ='Room 1';
 			users[socket.nickname]=socket;
-			//console.log(users[socket.nickname]);
+			socket.join('Room 1');
+			socket.emit('new message',{nick:'SERVER',msg:'You have connected to Room 1'});
+			socket.broadcast.to(socket.room).emit('new message',{nick:'SERVER',msg: data+' has connected to Room 1'});
+			socket.emit('updaterooms',rooms,'Room 1');
+			io.sockets.emit('usernames',Object.keys(users));
+			console.log(users[socket.nickname]);
 			updateNicknames();
+			updateUsers();
+			//roomusers[0].push(socket.nickname)
+
 			}
 	});
-
+	// function addImage(){
+	// 	fs.readFile(__dirname + '/images/image.jpg', function(err, buf){
+ //    // it's possible to embed binary data
+ //    // within arbitrarily-complex objects
+ //    	socket.emit('image', { image: true, buffer: buf});
+ //    			console.log('image file is initialized');
+ // 	 });
+	// }
 	function updateNicknames(){
 		//console.log(dataname);
 		//for (var i in Object.keys(users))
@@ -68,9 +88,17 @@ io.sockets.on('connection',function(socket){
         		if(ind !== -1){
           			var name = msg.substring(0,ind);
           			var msg = msg. substring(ind+1);
-          			if(name in users){
-          				users[name].emit('whisper',{msg:msg, nick:socket.nickname});
-          				users[socket.nickname].emit('own whisper',{msg:msg, nick:name});	
+          			if(name in users && users[name].room == socket.room ){
+          				if (name == socket.nickname)
+          					{
+          						users[name].emit('whisper',{msg:msg, nick:'ME'});
+          					}
+          				else
+          				{	
+          					users[name].emit('whisper',{msg:msg, nick:socket.nickname});
+          					users[socket.nickname].emit('own whisper',{msg:msg, nick:name});
+          				}
+          					
           				console.log('Whisper!');	
           				}
           			else{
@@ -86,15 +114,71 @@ io.sockets.on('connection',function(socket){
 			//newMsg.save(function(err){
 			//	if(err) throw err;
 			//io.sockets.emit('new message',{msg:msg, nick:socket.nickname});
-			socket.broadcast.emit('new message',{msg:msg, nick:socket.nickname});
+			socket.broadcast.to(socket.room).emit('new message',{msg:msg, nick:socket.nickname});
 			users[socket.nickname].emit('own message',{msg:msg, nick:socket.nickname});
-			//});
+			
+			//io.sockets.in(socket.room).emit('new message', {nick:socket.nickname, msg:msg});
+
+			//});for(var i = 0; i < clients.length; i++) {
+
 		     }
 		});
+	function updateUsers()
+	{
+		var roomusers = [[],[],[],[],[]];
 
+		for( var roo in rooms)
+		    	{	
+		    		console.log(roo);
+		    		console.log(rooms[roo]);
+		    		//var clients_no = io.sockets.clients[rooms[roo]]);
+				var client_no = io.sockets.adapter.rooms[rooms[roo]];
+				var clients = (typeof client_no !== 'undefined') ? io.sockets.adapter.rooms[rooms[roo]].sockets : [];
+				//var client_no = io.sockets.adapter.rooms[rooms[roo]].sockets;
+		    		console.log(clients);
+		    		for (var clientId in clients) 
+		    		{
+
+  					console.log('client: %s', clientId); //Seeing is believing 
+  					var client_socket = io.sockets.connected[clientId];
+  					console.log(client_socket.nickname);
+  					if (roomusers[roo].indexOf(client_socket.nickname) < 0)
+		    				roomusers[roo].push(client_socket.nickname);
+				}
+				console.log(roomusers);
+				io.sockets.to(rooms[roo]).emit('updateroomusers',roomusers[roo]);
+			}
+	}
+	socket.on('switchRoom', function(newroom){
+		    // leave the current room (stored in session)
+		    console.log("Here");
+		    socket.leave(socket.room);
+		    // join new room, received as function parameter
+		    socket.join(newroom);
+		    console.log("Here");
+		    var msg1 = 'You have connected to '+ newroom ;
+		    console.log(msg1);
+		    socket.emit('new message', {msg:msg1, nick:'SERVER'});
+		    // sent message to OLD room
+		    msg1 = socket.nickname +' has left this room';
+		    socket.broadcast.to(socket.room).emit('new message', { msg: msg1, nick:'SERVER'});
+		    // update socket session room title
+		    socket.room = newroom;
+		    msg1 = socket.nickname +' has joined this room';
+		    socket.broadcast.to(newroom).emit('new message', {msg:msg1, nick:'SERVER'});
+		    socket.emit('updaterooms', rooms, newroom);
+		    updateUsers();
+		    
+		
+  	});
+
+	
 	socket.on('disconnect',function(data){
 		if(!socket.nickname) return;
 		delete users[socket.nickname];
 		updateNicknames();
+		socket.broadcast.emit('new message', {nick:'SERVER',msg:socket.nickname + ' has disconnected'});
+    		socket.leave(socket.room);
+    		updateUsers();
 	});
 });
